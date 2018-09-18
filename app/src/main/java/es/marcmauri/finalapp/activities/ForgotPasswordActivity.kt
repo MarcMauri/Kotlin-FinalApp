24cd48725a1ef133a08c140b6cf205a94329bbd6
Marc from GitKraken <marcmauri92@gmail.com>
2018-09-18 15:48:17 +0200

    SignUp layout

--- app/src/main/java/es/marcmauri/finalapp/activities/ForgotPasswordActivity.kt
+++ app/src/main/java/es/marcmauri/finalapp/activities/ForgotPasswordActivity.kt
@@ -1,12 +1,13 @@
-package es.marcmauri.finalapp
+package es.marcmauri.finalapp.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import es.marcmauri.finalapp.R
 
-class LoginActivity : AppCompatActivity() {
+class ForgotPasswordActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_forgot_password)
     }
 }
