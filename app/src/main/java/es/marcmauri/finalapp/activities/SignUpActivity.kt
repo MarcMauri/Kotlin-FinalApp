dc2f0fb908e6dcb509423cef57c0d3551555c9c6
Marc from GitKraken <marcmauri92@gmail.com>
2018-09-18 22:58:59 +0200

    Sign up activity developed

--- app/src/main/java/es/marcmauri/finalapp/activities/SignUpActivity.kt
+++ app/src/main/java/es/marcmauri/finalapp/activities/SignUpActivity.kt
@@ -1,10 +1,14 @@
 package es.marcmauri.finalapp.activities
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import es.marcmauri.finalapp.R
+import es.marcmauri.finalapp.others.goToActivity
 import es.marcmauri.finalapp.others.toast
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 
 class SignUpActivity : AppCompatActivity() {
@@ -15,29 +19,39 @@ class SignUpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
-        // Check if user is signed in (non-null) and update UI accordingly.
-        val currentUser = mAuth.currentUser
-        if (currentUser === null) {
-            toast("User is NOT logged in")
-            createAccount("marcmauri92@gmail.com", "123456789")
-        } else {
-            toast("User IS logged in")
+        button_goLogIn.setOnClickListener {
+            goToActivity<LoginActivity> {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            }
         }
 
+        button_signUp.setOnClickListener {
+            val email = et_email.text.toString()
+            val password = et_password.text.toString()
+
+            if (isValidEmailAndPassword(email, password)) {
+                signUpByEmail(email, password)
+            } else {
+                toast("Please fill all the data and confirm password is correct.",
+                        Toast.LENGTH_LONG)
+            }
+        }
 
     }
 
-    private fun createAccount(email: String, password: String) {
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        // Sign in success, update UI with the signed-in user's information
-                        toast("createUserWithEmail:success")
-                        val user = mAuth.currentUser
-                    } else {
-                        // If sign in fails, display a message to the user.
-                        toast("createUserWithEmail:failure")
-                    }
-                }
+    private fun signUpByEmail(email: String, password: String) {
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                toast("An email has been sent to you. Please, confirm before sign in", Toast.LENGTH_LONG)
+            } else {
+                toast("An unexpected error occurred, please try again.")
+            }
+        }
+    }
+
+    private fun isValidEmailAndPassword(email: String, password: String): Boolean {
+        return !email.isNullOrEmpty() &&
+                !password.isNullOrEmpty() &&
+                password == et_confirmPassword.text.toString()
     }
 }
