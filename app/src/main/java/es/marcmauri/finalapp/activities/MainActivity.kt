8d4475921214ea413b65bcbae0daf764cf048d07
Marc from GitKraken <marcmauri92@gmail.com>
2018-09-20 15:22:25 +0200

    Bottom Navigation Bar created and light implemented

--- app/src/main/java/es/marcmauri/finalapp/activities/MainActivity.kt
+++ app/src/main/java/es/marcmauri/finalapp/activities/MainActivity.kt
@@ -1,9 +1,15 @@
 package es.marcmauri.finalapp.activities
 
 import android.os.Bundle
+import android.support.v4.view.ViewPager
 import android.support.v7.widget.Toolbar
+import android.view.MenuItem
 import com.google.firebase.auth.FirebaseAuth
 import es.marcmauri.finalapp.R
+import es.marcmauri.finalapp.adapters.PagerAdapter
+import es.marcmauri.finalapp.fragments.ChatFragment
+import es.marcmauri.finalapp.fragments.InfoFragment
+import es.marcmauri.finalapp.fragments.RatesFragment
 import es.marcmauri.mylibrary.ToolbarActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -11,10 +17,58 @@ class MainActivity : ToolbarActivity() {
 
     private val mAuth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
 
+    private lateinit var adapter: PagerAdapter
+    private var prevBottomSelected: MenuItem? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         toolbarToLoad(toolbarView as Toolbar)
+
+        setUpViewPager(getPagerAdapter())
+        setUpBottomNavigationBar()
+    }
+
+    private fun getPagerAdapter(): PagerAdapter {
+        adapter = PagerAdapter(supportFragmentManager)
+        adapter.addFragment(ChatFragment())
+        adapter.addFragment(InfoFragment())
+        adapter.addFragment(RatesFragment())
+        return adapter
+    }
+
+    private fun setUpViewPager(adapter: PagerAdapter) {
+        viewPager.adapter = adapter
+        viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {}
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
+            override fun onPageSelected(position: Int) {
+                if (prevBottomSelected == null) {
+                    bottomNavigation.menu.getItem(0).isChecked = false
+                } else {
+                    prevBottomSelected!!.isChecked = false
+                }
+                bottomNavigation.menu.getItem(0).isChecked = true
+                prevBottomSelected = bottomNavigation.menu.getItem(0)
+            }
+        })
+    }
+
+    private fun setUpBottomNavigationBar() {
+        bottomNavigation.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.bottom_nav_info -> {
+                    viewPager.currentItem = 0; true
+                }
+                R.id.bottom_nav_rates -> {
+                    viewPager.currentItem = 1; true
+                }
+                R.id.bottom_nav_chat -> {
+                    viewPager.currentItem = 2; true
+                }
+                else -> false
+            }
+        }
     }
 }
