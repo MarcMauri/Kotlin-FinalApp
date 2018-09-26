bc7ac607b71bdbea8a28d7c22ddd95a98dc780d8
Marc from GitKraken <marcmauri92@gmail.com>
2018-09-26 14:45:15 +0200

    Rate model

---
+++ app/src/main/java/es/marcmauri/finalapp/models/Rate.kt
@@ -0,0 +1,10 @@
+package es.marcmauri.finalapp.models
+
+import java.util.*
+
+data class Rate(
+        val text: String,
+        val rate: Float,
+        val createdAt: Date,
+        val profileImgURL: String = ""
+)
