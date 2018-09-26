8929a99e0b36bf62d6ea45aab091ba1bede06158
Marc from GitKraken <marcmauri92@gmail.com>
2018-09-26 18:03:18 +0200

    Layout fragment_rates_item enhanced

--- app/src/main/java/es/marcmauri/finalapp/adapters/RatesAdapter.kt
+++ app/src/main/java/es/marcmauri/finalapp/adapters/RatesAdapter.kt
@@ -17,13 +17,13 @@ class RatesAdapter(private val items: List<Rate>) : RecyclerView.Adapter<RatesAd
     override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(items[position])
     override fun getItemCount() = items.size
 
-    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(rate: Rate) = with(itemView) {
             tv_rate.text = rate.text
             tv_star.text = "${rate.rate}"
             tv_calendar.text = SimpleDateFormat("dd MMM, yyyy").format(rate.createdAt)
 
-            if(rate.profileImgURL.isEmpty()) {
+            if (rate.profileImgURL.isEmpty()) {
                 Picasso.get().load(R.drawable.ic_person).resize(100, 100)
                         .centerCrop().transform(CircleTransform()).into(iv_profile)
             } else {
